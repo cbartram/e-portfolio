0677c6157dd294e53daae26e94c745640b438272
cbartram <cbartram3@gmail.com>
2018-03-24 09:37:45 -0400

    Added react router for page routing

--- client/e-portfolio/src/index.js
+++ client/e-portfolio/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={App} />
+            </Switch>
+        </BrowserRouter>, document.getElementById('root'));
+
 registerServiceWorker();
