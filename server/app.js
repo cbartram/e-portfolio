274d13faed63da9290f722fc34bc145ab1bc4ab5
cbartram <cbartram3@gmail.com>
2018-03-28 12:25:53 -0400

    Added Auth in NodeJS and created Login Component

--- server/app.js
+++ server/app.js
@@ -1,22 +1,80 @@
-let express = require('express');
-let path = require('path');
-let logger = require('morgan');
-let cookieParser = require('cookie-parser');
-let bodyParser = require('body-parser');
+require('dotenv').config();
+const moment = require('moment');
+const bcrypt = require('bcrypt-nodejs');
+const _ = require('lodash');
+const uuid = require('node-uuid');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const AWS = require('aws-sdk');
+const session = require("express-session");
+const passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
 
-let index = require('./routes/index');
+//Configure AWS
+AWS.config.update({region:'us-east-1', secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET, accessKeyId: process.env.AWS_ACCESS_KEY_ID});
+
+//Init DB Connection
+let dynamodb = new AWS.DynamoDB();
+
+passport.use(new LocalStrategy((username, password, done) => {
+     let query = {
+            Key: {
+                "username": {
+                    S: username.toUpperCase()
+                }
+            },
+            TableName: "Users"
+        };
+
+        dynamodb.getItem(query, (err, data) => {
+            if (err) {
+                return done(null, { message: 'Server Error Encountered' });
+            } else {
+                if(_.isEmpty(data)) {
+                    return done(null, { message: 'Incorrect Username' });
+                } else {
+
+                    if(bcrypt.compareSync(password,  data.Item.password.S)) {
+                        return done(null, {username: data.Item.username.S})
+                    }
+                    return done(null, { message: 'Incorrect Password'})
+                }
+            }
+        });
+    }
+));
+
+/**
+ * Serializes user to the http Session
+ */
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+/**
+ * Deserializes user to the http session
+ */
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+const index = require('./routes/index');
 
 let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: "treepepperonicupjacketphonedogkingbreadcakelovetourbignoonsuperzoom" }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Allow CORS
 app.use((req, res, next) => {
@@ -26,7 +84,7 @@ app.use((req, res, next) => {
     next();
 });
 
-//Allow for the Preflight options request before
+//Allow for the Preflight options request before the post request
 app.options("/*", function(req, res){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -40,14 +98,45 @@ app.options("/*", function(req, res){
  * =================
  */
 app.use('/', index);
+app.post('/admin/login', passport.authenticate('local'), (req, res) => {
+        res.json({success: true, user: req.user})
+});
+
 app.post('/contact/submit', (req, res) => {
-    console.log("Running");
+    const id = uuid.v4();
+
+    let query = {
+        Item: {
+            "messageID": {
+                S: id
+            },
+            "title": {
+                S: req.body.title
+            },
+            "message": {
+                S: req.body.message
+            },
+            "email": {
+                S: req.body.email
+            },
+            "timestamp": {
+                S: moment().format("YYYY-DD-MM h:mm:ss")
+            }
+        },
+        ReturnConsumedCapacity: "TOTAL",
+        TableName: "Messages"
+    };
+
+    dynamodb.putItem(query, (err, data) => {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(data);           // successful response
+    });
+
     res.json({success: true, body: req.body})
 });
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-    console.log("Running first");
   let err = new Error('Not Found');
   err.status = 404;
   next(err);
---
+++ src/components/pages/Login/Login.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+
+export default class Login extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+          form: {
+              username: '',
+              password: '',
+          },
+          error: {
+              username: '',
+              password: ''
+          }
+        };
+    }
+
+    handleSubmit = () => {
+        let {form, error} = this.state;
+        let err = false;
+
+        for(let prop in form) {
+            if(form.hasOwnProperty(prop)) {
+                if(form[prop].length <= 0) {
+                    //Throw error message
+                    console.error("One of the fields is empty");
+                    err = true;
+                }
+            }
+        }
+
+        //Submit the form
+        //TODO CHANGE TO THE DEST URL
+        !err &&
+        fetch('http://localhost:3001/admin/login', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json', 'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.state),
+        }).then(res => res.json()).then(body => {
+            //Show error/success messages
+            if(typeof body.user.message !== 'undefined') {
+                if(body.user.message.includes('Password')) {
+                    this.setState({error: {username: '', password: body.user.message}})
+                } else {
+                    this.setState({error: {username: body.user.message, password: ''}})
+                }
+            } else {
+                //Successful Login
+                console.log("Successful Login!");
+                this.setState({ error: {username: '', password: ''}})
+            }
+        });
+    };
+
+    handleChange = (value, type) => {
+        let { form } = this.state;
+        form[type] = value;
+        this.setState(form)
+    };
+
+    render() {
+        return (
+            <div className="container">
+                <div className="col-md-4 col-md-offset-3">
+                    <h2>Admin Login</h2>
+                </div>
+                <div className="row">
+                    <div className="col-md-6 col-md-offset-3">
+                        <TextField floatingLabelText="Username" errorText={this.state.error.username} value={this.state.form.username} onChange={e => this.handleChange(e.target.value, 'username')}/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6 col-md-offset-3">
+                        <TextField floatingLabelText="Password" type="password" errorText={this.state.error.password} value={this.state.form.password} onChange={e => this.handleChange(e.target.value, 'password')}/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6 col-md-offset-3">
+                        <RaisedButton label="Login" onClick={this.handleSubmit}/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
